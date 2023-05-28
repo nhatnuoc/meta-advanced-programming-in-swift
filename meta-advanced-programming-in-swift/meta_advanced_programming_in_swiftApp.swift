16a8c9f5879b4580c43d84c0b68cf5703f56adcd
Nguyen Thanh Bình <binhvuong.2010@gmail.com>
2023-05-28 15:07:38 +0700

    Initial Commit

---
+++ meta-advanced-programming-in-swift/meta_advanced_programming_in_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  meta_advanced_programming_in_swiftApp.swift
+//  meta-advanced-programming-in-swift
+//
+//  Created by Nguyễn Thanh Bình on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct meta_advanced_programming_in_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
